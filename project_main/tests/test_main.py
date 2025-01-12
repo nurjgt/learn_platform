86ab6a193e7ddd705cd927eeb2c0eacdc536ca39
nurjgt <nurzhigit.khovdabay@mail.ru>
2025-01-12 23:44:21 +0100

    Nurzhigit 5.0

---
+++ project_main/tests/test_main.py
@@ -0,0 +1,7 @@
+def test_example_function():
+    from main import example_function
+    assert example_function() == expected_value
+
+def test_another_function():
+    from main import another_function
+    assert another_function(arg) == expected_result
